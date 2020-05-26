9a1cb6865041b6b20fc62653caccae0fa08b6026
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-26 10:54:24 -0300

    Criado corpo da aplicação, estilizando a lista de produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,90 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/8812880a9b3a4abb64e4ba6284d18fd6.png"
+          alt="MacBook Pro"
+        />
+        <strong>MacBook Pro</strong>
+        <span>R$11.000,29</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/8812880a9b3a4abb64e4ba6284d18fd6.png"
+          alt="MacBook Pro"
+        />
+        <strong>MacBook Pro</strong>
+        <span>R$11.000,29</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/8812880a9b3a4abb64e4ba6284d18fd6.png"
+          alt="MacBook Pro"
+        />
+        <strong>MacBook Pro</strong>
+        <span>R$11.000,29</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/8812880a9b3a4abb64e4ba6284d18fd6.png"
+          alt="MacBook Pro"
+        />
+        <strong>MacBook Pro</strong>
+        <span>R$11.000,29</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://assets.xtechcommerce.com/uploads/images/medium/8812880a9b3a4abb64e4ba6284d18fd6.png"
+          alt="MacBook Pro"
+        />
+        <strong>MacBook Pro</strong>
+        <span>R$11.000,29</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
